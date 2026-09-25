c216dc5362f31dff4b32077b808d2703545aa770
28TMurray <28tmurray@live.ytech.edu>
2026-09-25 14:57:26 -0400

    Tiny Migration Progress

--- database/migrations/JSMigrations/45_combine_trade_transaction.js
+++ database/migrations/JSMigrations/45_combine_trade_transaction.js
@@ -6,11 +6,13 @@ module.exports = {
         const tradesTable = await dbGet("SELECT name FROM sqlite_master WHERE type='table' AND name='trades'");
         if (!tradesTable) return;
 
-        const transactionRows = await dbGetAll("SELECT * FROM transactionEntries");
+        const transactionRows = await dbGetAll("SELECT * FROM transaction");
         
         const combinedTransactions = transactionRows.map((transaction) => {
             return {
-                fromTopHolderId: 0
+                fromUserId: transaction.from_id,
+                fromType: transaction.fromType,
+                fromPool
             }
         })
         const tradeRows = await dbGetAll("SELECT * FROM trades");
